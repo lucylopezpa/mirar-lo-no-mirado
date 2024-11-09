6a6c8478b48364687e3c7ada9e9decd78a299cef
David Daza <03dazal@gmail.com>
2024-11-09 08:01:46 -0500

    rewrite navbar as astro component

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -2,32 +2,6 @@ import Swiper from "swiper";
 import { Keyboard, Mousewheel, Pagination } from "swiper/modules";
 import type { SwiperOptions } from "swiper/types";
 
-const rooms = document.querySelectorAll(".swiper-room");
-const roomsNav = document.getElementById("rooms-nav");
-const root = document.documentElement;
-const roomsAnchor = Array.from(document.querySelectorAll(".js-room-anchor"));
-
-const tabs = new Map<string, HTMLButtonElement>();
-// const swipers = new Map<string, Swiper>()
-
-const roomsObserverCallback: IntersectionObserverCallback = (entries) => {
-  entries.forEach((entry) => {
-    const roomId = (entry.target as HTMLElement).id;
-    if (!tabs.has(roomId)) {
-      const selector = `[data-room=${roomId}]`;
-      tabs.set(roomId, roomsNav?.querySelector(selector) as HTMLButtonElement);
-    }
-    tabs.get(roomId)?.classList.toggle("font-bold", entry.isIntersecting);
-    if (roomId === "room-3") {
-      root.classList.toggle("dark", entry.isIntersecting);
-    }
-  });
-};
-
-const observer = new IntersectionObserver(roomsObserverCallback, {
-  threshold: 0.1,
-});
-
 const options: SwiperOptions = {
   modules: [Mousewheel, Keyboard, Pagination],
   direction: "horizontal",
@@ -42,7 +16,50 @@ const options: SwiperOptions = {
   },
 };
 
+const mobileMenu = document.getElementById("mobile-menu");
+const rooms = document.querySelectorAll(".swiper-room");
+const roomsNav = document.getElementById("rooms-nav");
+const root = document.documentElement;
+const roomsAnchor = Array.from(document.querySelectorAll(".js-room-anchor"));
+
+const tabs = new Map<string, HTMLButtonElement>();
+
 const swiper = new Swiper(".swiper-root", options);
+
+// Handle room navigation from navbar menu
+if (location.hash) {
+  console.log();
+
+  const room = location.hash.slice(-1);
+  if (!Number.isNaN(room)) {
+    // Note this is hacky, but it does the job
+    const slide = +room + 2;
+    swiper.slideTo(slide);
+    closeMenuAfterNavigation()
+  }
+}
+
+const roomsObserverCallback: IntersectionObserverCallback = (entries) => {
+  entries.forEach((entry) => {
+    const roomId = (entry.target as HTMLElement).id;
+    if (!tabs.has(roomId)) {
+      const selector = `[data-room=${roomId}]`;
+      tabs.set(roomId, roomsNav?.querySelector(selector) as HTMLButtonElement);
+    }
+    tabs.get(roomId)?.classList.toggle("font-bold", entry.isIntersecting);
+    if (roomId === "room-3") {
+      root.classList.toggle("dark", entry.isIntersecting);
+    }
+    // Handle rooms nav visibility on external navigation
+    if (entry.isIntersecting && roomsNav?.className.includes("opacity-0")) {
+      roomsNav.classList.remove("opacity-0");
+    }
+  });
+};
+
+const observer = new IntersectionObserver(roomsObserverCallback, {
+  threshold: 0.1,
+});
 
 rooms.forEach((room) => {
   const swiper = new Swiper(room as HTMLElement, {
@@ -71,9 +88,20 @@ roomsAnchor.forEach((el) => {
   el.addEventListener("click", () => {
     const slide = (el as HTMLElement).dataset.slide as string;
     swiper.slideTo(+slide);
+    closeMenuAfterNavigation()
   });
 });
 
-swiper.on("slideChange", ({ activeIndex }) => {
+swiper.on("slideChange", ({ activeIndex, realIndex }) => {
   roomsNav?.classList.toggle("opacity-0", activeIndex < 3);
 });
+
+window.addEventListener('popstate', function (e) {
+  closeMenuAfterNavigation()
+})
+
+function closeMenuAfterNavigation() {
+  if (!mobileMenu?.className.includes("hidden")) {
+    mobileMenu?.classList.add("hidden");
+  }
+}
