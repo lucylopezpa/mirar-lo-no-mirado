f4e25c35b8e115db61c502f09e274da6673754bb
David Daza <03dazal@gmail.com>
2024-11-10 16:34:21 -0500

    animate rooms cover with scrollmagic

--- src/components/Layered.tsx
+++ src/components/Layered.tsx
@@ -1,22 +1,32 @@
-import { useRef, useState, type FC } from "react"
+import { useEffect, useRef, useState, type FC } from "react"
 import { useIntersectionObserver } from "usehooks-ts";
+import ScrollMagic from 'scrollmagic'
 
 const Layered: FC<{ background: string; foreground: string; alt?: string }> = ({ background, foreground, alt = "" }) => {
+  const controller = new ScrollMagic.Controller({
+    vertical: false,
+  })
   const foregroundRef = useRef<HTMLImageElement>(null)
 
-  const { ref } = useIntersectionObserver({
-    threshold: [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1],
-    onChange(isIntersecting, entry) {
-      if (!isIntersecting) return
-      const opacity = (1 - entry.intersectionRatio) / 0.2
-      if (foregroundRef.current) {
-        foregroundRef.current.style.opacity = opacity + ''
-      }
+  useEffect(() => {
+    if (!foregroundRef.current) return
+    const scene = new ScrollMagic.Scene({
+      triggerElement: foregroundRef.current,
+      duration: '10%',
+      triggerHook: 'onLeave',
+    })
+    .addTo(controller)
+    scene.on("progress", function(event) {
+      // @ts-expect-error scrollmagic event is not _typed_
+      foregroundRef.current.style.opacity = event.progress
+    })
+    return () => {
+      scene.destroy()
     }
-  })
+  }, [foregroundRef])
 
   return (
-    <div className="relative" ref={ref}>
+    <div className="relative">
       <img src={background} alt={alt} />
       <img src={foreground} className="absolute top-0 left-0" style={{ opacity: '0' }} ref={foregroundRef} alt={alt} />
     </div>
